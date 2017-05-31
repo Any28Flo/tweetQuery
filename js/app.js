59ec0c5a1dc21312a4e84fb275efc7a89ad37fe4
“Erika <foglzerika@gmail.com>
2017-05-31 00:58:34 -0500

    Creamd textArea y button

---
+++ js/app.js
@@ -0,0 +1,10 @@
+// Seleccionamos al elemento con el cual vamos a trabajar
+var cargarPagina= function(){
+
+	var $areaTrabajo=$("#elementos");
+	console.log("hola");
+};
+
+
+// Una vez que la pagina carge que ejecute la funcion
+window.addEventListener("load", cargarPagina);
