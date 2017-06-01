8396ce3398ec80e4772e0b64a044b3326859ca8c
“Erika <foglzerika@gmail.com>
2017-06-01 16:07:21 -0500

    Agregando funcionalidad de el textArea no este vacia

--- js/appTweet.js
+++ js/appTweet.js
@@ -1,16 +1,23 @@
 
+var $elemento = $("#btnEnviar");
 
 var insertaComentario = function(evento){
 	evento.preventDefault();
 	var $contenido = $("#comentario").val();
-	console.log($contenido);
-	var $comentario= $("#lista");
-	$comentario.append("<p>"+$contenido+"</p>");
+	if ($contenido ===""){
+		alert("Inserta texto antes chavo!!");
+
+	}else{
+		var $comentario= $("#lista");
+		$comentario.append("<p>"+$contenido+"</p>");	
+	}
+	
 
 }
+var desac
 var cargaPagina = function(){
 
-	var $elemento = $("#btnEnviar");
+	
 	$elemento.click(insertaComentario);
 }
 
